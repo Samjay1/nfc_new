c1ed7c60541f28e172f8a03be2d5a266e49ba798
Samjay <barnabassampana89@gmail.com>
2024-08-30 12:25:57 +0000

    pus

--- lib/Reader.dart
+++ lib/Reader.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 class Reader extends StatefulWidget {
   const Reader({super.key});
@@ -31,14 +32,21 @@ class _ReaderState extends State<Reader> {
 
       //We first check if NFC is available on the device.
       if (isAvailable) {
+        NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
+          print(tag.data['isodep']);
+          print(tag.data['nfcb']);
+
+          IsoDep? isoDep = IsoDep.from(tag);
+          print(isoDep?.historicalBytes);
+        });
         //If NFC is available, start an NFC session and listen for NFC tags to be discovered.
-        NfcManager.instance.startSession(
-          onDiscovered: (NfcTag tag) async {
-            // Process NFC tag, When an NFC tag is discovered, print its data to the console.
-            debugPrint('NFC Tag Detected: ${tag.data}');
-            if (context.mounted) ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('NFC Tag Detected: ${tag.data}')));
-          },
-        );
+        // NfcManager.instance.startSession(
+        //   onDiscovered: (NfcTag tag) async {
+        //     // Process NFC tag, When an NFC tag is discovered, print its data to the console.
+        //     debugPrint('NFC Tag Detected: ${tag.data}');
+        //     if (context.mounted) ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('NFC Tag Detected: ${tag.data}')));
+        //   },
+        // );
       } else {
         debugPrint('NFC not available.');
         if (context.mounted) ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('NFC not available.')));
